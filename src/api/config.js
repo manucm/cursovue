bee2ae45abf2e60cb3b50c73a868720203a0ba2b
manuelprg <manuel.contreras.morillo@gmail.com>
2017-05-31 00:24:05 +0200

    commit inicial. Con el esqueleto del proyecto, dependencia de pug, dependencias de stylus, y acceso a la api.

---
+++ src/api/config.js
@@ -0,0 +1,10 @@
+export default {
+	appName: 'mmusic',
+	apiKey: '75a1a6353b3e25ffdc41ab5ef6e4e852',
+	secret: 'a6744ed96cf57cc150ba1921f17fd25b',
+	registerdTo: 'quimtreras'
+}
+//Application name	mmusic
+//API key	75a1a6353b3e25ffdc41ab5ef6e4e852
+//Shared secret	a6744ed96cf57cc150ba1921f17fd25b
+//Registered to	quimtreras
