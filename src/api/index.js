e3ae0366f69ab8fbf96ac36219f10efd463f4521
manuelprg <manuel.contreras.morillo@gmail.com>
2017-06-01 09:47:59 +0200

    url a la api https

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import config from './config.js'
 //const apiKey = config.apiKey; es igual que escribir lo siguiente
 const { apiKey } = config
-const URL = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${apiKey}&format=json`;
+const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${apiKey}&format=json`;
 
 export default function getArtists(country) {
 	const url = URL.replace(':country', country)
